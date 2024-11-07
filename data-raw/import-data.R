38634bb6ab8ec8512c6c578ddecf7d7962c72ca4
David Keyes <david@rfortherestofus.com>
2024-11-07 11:01:02 -0800

    Create project

--- data-raw/import-data.R
+++ data-raw/import-data.R
@@ -1,23 +1,19 @@
 # Load Packages -----------------------------------------------------------
 
 library(tidyverse)
+library(fs)
 
+# Create vector of CSVs ---------------------------------------------------
+
+csv_files <-
+  dir_ls(
+    path = "data-raw",
+    regexp = "csv"
+  )
 
 # Import Data -------------------------------------------------------------
 
-# read_csv("data-raw/survey_data_2021.csv") |>
-#   view()
-
-# demographics <-
-#   read_csv("data-raw/survey_data_2021.csv") |>
-#   select(respondent_id, location) |>
-#   separate_wider_delim(
-#     cols = location,
-#     delim = ", ",
-#     names = c("city", "state")
-#   )
-
-import_demographics_data <- function(csv_file, year) {
+import_demographics_data <- function(csv_file) {
   read_csv(csv_file) |>
     select(respondent_id, location) |>
     separate_wider_delim(
@@ -25,104 +21,90 @@ import_demographics_data <- function(csv_file, year) {
       delim = ", ",
       names = c("city", "state")
     ) |>
-    mutate(survey_year = year)
+    mutate(survey_year = csv_file) |>
+    mutate(survey_year = str_remove(survey_year, "data-raw/survey_data_")) |>
+    mutate(survey_year = str_remove(survey_year, ".csv")) |>
+    mutate(survey_year = as.numeric(survey_year))
 }
 
-demographics_2021 <- import_demographics_data(
-  csv_file = "data-raw/survey_data_2021.csv",
-  year = 2021
-)
-demographics_2022 <- import_demographics_data(
-  csv_file = "data-raw/survey_data_2022.csv",
-  year = 2022
-)
-demographics_2023 <- import_demographics_data(
-  csv_file = "data-raw/survey_data_2023.csv",
-  year = 2023
-)
-demographics_2024 <- import_demographics_data(
-  csv_file = "data-raw/survey_data_2024.csv",
-  year = 2024
-)
+demographics_data <-
+  map(csv_files, import_demographics_data) |>
+  bind_rows()
 
-demographics <-
-  bind_rows(
-    demographics_2021,
-    demographics_2022,
-    demographics_2023,
-    demographics_2024
-  )
+demographics_data |> 
+  write_rds("data/demographics_data.rds")
 
-import_favorite_parts_data <- function(csv_file, year) {
+import_favorite_parts_data <- function(csv_file) {
   read_csv(csv_file) |>
     select(respondent_id, favorite_parts) |>
     separate_longer_delim(
       cols = favorite_parts,
       delim = ", "
     ) |>
-    mutate(survey_year = year)
+    rename(favorite_part = favorite_parts) |>
+    mutate(survey_year = csv_file) |>
+    mutate(survey_year = str_remove(survey_year, "data-raw/survey_data_")) |>
+    mutate(survey_year = str_remove(survey_year, ".csv")) |>
+    mutate(survey_year = as.numeric(survey_year))
 }
 
-favorite_parts_2021 <-
-  import_favorite_parts_data(
-    csv_file = "data-raw/survey_data_2021.csv",
-    year = 2021
-  )
+favorite_parts_data <-
+  map(csv_files, import_favorite_parts_data) |>
+  bind_rows()
 
-favorite_parts_2022 <-
-  import_favorite_parts_data(
-    csv_file = "data-raw/survey_data_2022.csv",
-    year = 2022
-  )
+favorite_parts_data |> 
+  write_rds("data/favorite_parts_data.rds")
 
-favorite_parts_2023 <-
-  import_favorite_parts_data(
-    csv_file = "data-raw/survey_data_2023.csv",
-    year = 2023
-  )
+import_pre_post_data <- function(csv_file) {
+  read_csv(csv_file) |>
+    select(respondent_id, pre_question_1:post_question_2) |>
+    pivot_longer(
+      cols = -respondent_id
+    ) |>
+    separate_wider_delim(
+      cols = name,
+      delim = "_",
+      names = c("timing", "question", "question_number")
+    ) |>
+    select(-question) |>
+    mutate(survey_year = csv_file) |>
+    mutate(survey_year = str_remove(survey_year, "data-raw/survey_data_")) |>
+    mutate(survey_year = str_remove(survey_year, ".csv")) |>
+    mutate(survey_year = as.numeric(survey_year))
+}
 
-favorite_parts_2024 <-
-  import_favorite_parts_data(
-    csv_file = "data-raw/survey_data_2024.csv",
-    year = 2024
-  )
+pre_post_data <-
+  map(csv_files, import_pre_post_data) |>
+  bind_rows()
 
-favorite_parts <-
-  bind_rows(
-    favorite_parts_2021,
-    favorite_parts_2022,
-    favorite_parts_2023,
-    favorite_parts_2024
-  )
+pre_post_data |> 
+  write_rds("data/pre_post_data.rds")
 
-pre_post_questions <-
-  read_csv("data-raw/survey_data_2022.csv") |>
-  select(respondent_id, pre_question_1:post_question_2) |>
-  pivot_longer(
-    cols = -respondent_id
-  ) |>
-  separate_wider_delim(
-    cols = name,
-    delim = "_",
-    names = c("timing", "question", "question_number")
-  ) |>
-  select(-question)
+import_satisfaction_data <- function(csv_file) {
+  
+    read_csv(csv_file) |>
+    select(respondent_id, contains("satisfaction")) |>
+    pivot_longer(
+      cols = -respondent_id
+    ) |>
+    separate_wider_delim(
+      cols = name,
+      delim = "_",
+      names = c("question_type", "question", "question_number")
+    ) |>
+    select(-c(question_type, question)) |> 
+    mutate(survey_year = csv_file) |>
+    mutate(survey_year = str_remove(survey_year, "data-raw/survey_data_")) |>
+    mutate(survey_year = str_remove(survey_year, ".csv")) |>
+    mutate(survey_year = as.numeric(survey_year))
+}
 
-satisfaction_questions <-
-  read_csv("data-raw/survey_data_2022.csv") |>
-  select(respondent_id, contains("satisfaction")) |>
-  pivot_longer(
-    cols = -respondent_id
-  ) |>
-  separate_wider_delim(
-    cols = name,
-    delim = "_",
-    names = c("question_type", "question", "question_number")
-  ) |>
-  select(-c(question_type, question))
+satisfaction_data <-
+  map(csv_files, import_satisfaction_data) |>
+  bind_rows()
 
-
-
+satisfaction_data |> 
+  write_rds("data/satisfaction_data.rds")
 
 # Examples ----------------------------------------------------------------
 
